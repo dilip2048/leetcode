451c013c0bb2853c6fc73b05dd8e8d1d82be7f11
dilip2048 <dilipptt@gmail.com>
2022-10-21 09:08:38 -0500

    comment

--- 0053.Maximum_Subarray/maximum_subarray.go
+++ 0053.Maximum_Subarray/maximum_subarray.go
@@ -4,7 +4,7 @@ func maxSubArray(nums []int) int {
 	maxSum := nums[0]
 	currSum := 0
 	for i := 0; i < len(nums); i++ {
-		// it wont contribute in finding the max array
+		// it won't contribute in finding the max array
 		if currSum < 0 {
 			currSum = 0
 		}
